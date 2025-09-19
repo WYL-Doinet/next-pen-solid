fac32ddb2d11caccb0d79de537a31dd035c761b9
WYL <mmotashibana@gmail.com>
2025-09-19 22:28:36 +0900

    fix

--- src/components/editor/editor-canvas.tsx
+++ src/components/editor/editor-canvas.tsx
@@ -5,8 +5,8 @@ interface EditorCanvasProps {
 export default function EditorCanvas(props: EditorCanvasProps) {
     return (
         <div class="flex-1 bg-gray-100 p-2.5 overflow-hidden ">
-            <div ref={props.ref} class="w-full h-full  flex justify-center overflow-hidden  items-center rounded">
-                <canvas id="c"></canvas>
+            <div ref={props.ref} class="w-full h-full flex justify-center overflow-hidden  items-center rounded">
+                <canvas id="c" class=" transition duration-100"></canvas>
             </div>
         </div>
     );
